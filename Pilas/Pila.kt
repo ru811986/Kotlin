89a061f0367271b0cd5fb6ba788a07237c73e4f1
Rubén Rafael Galán Sanz <ru811986@hotmail.com>
2020-06-18 22:27:40 +0200

    Create Pila.kt
    Clase Pila para la creación de pilas

---
+++ Pilas/Pila.kt
@@ -0,0 +1,71 @@
+package pilas
+
+class Pila(var cima: Nodo?) {
+    fun estaVacia(): Boolean = if (cima == null) true else false
+    fun devuelveLongitud(): Int {
+        var actual: Nodo? = cima
+        var longitud: Int = 0
+        if(!estaVacia()) {
+            while (actual != null) {
+                longitud++
+                actual = actual.siguiente
+            }
+        }
+        return longitud
+    }
+    fun apilar(datoAApilar: Int) {
+        if(estaVacia()) {
+            var nuevo: Nodo? = Nodo(datoAApilar, null)
+            cima = nuevo
+        } else {
+            var nuevo: Nodo? = Nodo(datoAApilar, cima)
+            cima = nuevo
+        }
+        println("Elemento $datoAApilar apilado correctamente")
+    }
+    fun buscar(datoABuscar: Int) {
+        var actual: Nodo? = cima
+        var encontrado: Boolean = false
+        var posicion: Int = 0
+        if(!estaVacia()) {
+            while(actual != null) {
+                if(actual.dato == datoABuscar) {
+                    println("Elemento $datoABuscar encontrado en la posición $posicion")
+                    encontrado = true
+                }
+                posicion++
+                actual = actual.siguiente
+            }
+        }
+    }
+    fun modificar(datoAModificar: Int, datoModificado: Int) {
+        var actual: Nodo? = cima
+        var modificado: Boolean = false
+        var posicion: Int = 0
+        if(!estaVacia()) {
+            while(actual != null) {
+                if(actual.dato == datoAModificar) {
+                    actual.dato = datoModificado
+                    println("Elemento $datoAModificar modificado por $datoModificado en la posición $posicion")
+                    modificado = true
+                }
+                posicion++
+                actual = actual.siguiente
+            }
+        }
+    }
+    fun desapilar() {
+        var nodoAEliminar: Nodo? = cima
+        cima = cima?.siguiente
+        println("Elemento ${nodoAEliminar?.dato} desapilado correctamente")
+    }
+    fun mostrar() {
+        var actual: Nodo? = cima
+        if(!estaVacia()) {
+            while(actual != null) {
+                println(actual.dato)
+                actual = actual.siguiente
+            }
+        }
+    }
+}
